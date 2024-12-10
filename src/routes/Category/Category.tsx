bcb4d6c6426c47361111567ed63f45f1e6abd783
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-12-10 18:58:06 +0100

    Css adjustements

--- src/routes/Category/Category.tsx
+++ src/routes/Category/Category.tsx
@@ -1,14 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import styles from "./style.module.css";
+import { IoArrowBack } from "react-icons/io5";
 import { categories, tracks } from "../../data/mocks";
 import { CategoryType, TrackType } from "../../@types/TrackType";
 import CategoryData from "./CategoryData/CategoryData";
 import CategorySidebar from "../../components/CategorySidebar/CategorySidebar";
 import TrackList from "../../components/TrackList/TrackList";
+import styles from "./style.module.css";
 
 function Category() {
   const params = useParams();
+  const navigate = useNavigate();
   const [tracksList, setTracksList] = useState<TrackType[] | undefined>();
   const [currentCat, setCurrentCat] = useState<CategoryType>();
 
@@ -24,9 +26,16 @@ function Category() {
     }
   }, [params.id]);
 
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <main id={styles.tracksPage}>
-      <h1>{currentCat?.name}</h1>
+      <div id={styles.header}>
+        <IoArrowBack id={styles.backButton} onClick={handleBack} />{" "}
+        <h1>{currentCat?.name}</h1>
+      </div>
       <div id={styles.main}>
         <CategoryData category={currentCat} tracksNumber={tracksList?.length} />
         <TrackList tracks={tracksList} />
