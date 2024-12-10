c93396ccae10a3eafb96b04ec2a1394825a36dd2
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-12-10 11:03:22 +0100

    Add mocks and models

---
+++ src/@types/TrackType.ts
@@ -0,0 +1,13 @@
+export type TrackType = {
+  id: number;
+  title: string;
+  artist: string;
+  categoryId: number;
+  thumbnail: string;
+};
+
+export type CategoryType = {
+  id: number;
+  name: string;
+  thumbnail: string;
+};
---
+++ src/data/mocks.ts
@@ -0,0 +1,85 @@
+import { CategoryType, TrackType } from "../@types/TrackType";
+
+export const tracks: TrackType[] = [
+  {
+    id: 1,
+    title: "Ganges warriors",
+    artist: "Royalty Free Music",
+    categoryId: 1,
+    thumbnail: "warrior.jpeg",
+  },
+  {
+    id: 2,
+    title: "Six Strings",
+    artist: "Majed Salih",
+    categoryId: 1,
+    thumbnail: "six.jpeg",
+  },
+  {
+    id: 3,
+    title: "Noise",
+    artist: "Praz Khanal",
+    categoryId: 1,
+    thumbnail: "noise.jpeg",
+  },
+  {
+    id: 4,
+    title: "Shadowrun",
+    artist: "Tab",
+    categoryId: 1,
+    thumbnail: "shadowrun.jpeg",
+  },
+];
+
+export const categories: CategoryType[] = [
+  {
+    id: 1,
+    name: "Beatmaker's arena",
+    thumbnail: "beatmaker.jpg",
+  },
+  {
+    id: 2,
+    name: "Chill zone",
+    thumbnail: "chill.jpg",
+  },
+  {
+    id: 3,
+    name: "Time to dream",
+    thumbnail: "dream.jpg",
+  },
+  {
+    id: 4,
+    name: "Electro waves",
+    thumbnail: "electro.jpg",
+  },
+  {
+    id: 5,
+    name: "Indie",
+    thumbnail: "indie.jpg",
+  },
+  {
+    id: 6,
+    name: "Cool instrumentals",
+    thumbnail: "instru.jpg",
+  },
+  {
+    id: 7,
+    name: "Metal",
+    thumbnail: "metal.jpg",
+  },
+  {
+    id: 8,
+    name: "Just piano",
+    thumbnail: "piano.jpg",
+  },
+  {
+    id: 9,
+    name: "On the rocks",
+    thumbnail: "rock.jpg",
+  },
+  {
+    id: 10,
+    name: "Urban",
+    thumbnail: "urban.jpg",
+  },
+];
